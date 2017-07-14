2e82d6aadfd06e5e75501fae0ef999e4814d7ee6
1 <martin@pixelpudu.com>
2017-07-14 13:53:02 +0100

    fix

--- functions.php
+++ functions.php
@@ -347,6 +347,29 @@ add_filter('widget_text','php_execute',100);
 =            INCLUDES            =
 ================================*/
 
+add_action( 'admin_init', 'my_remove_menu_pages' );
+function my_remove_menu_pages() {
+ 
+    $user = wp_get_current_user();
+	if ( in_array( 'contributor', (array) $user->roles ) ) {
+		// remove_menu_page('edit.php'); // Posts
+		remove_menu_page('upload.php'); // Media
+		remove_menu_page('link-manager.php'); // Links
+		remove_menu_page('edit-comments.php'); // Comments
+		remove_menu_page('edit.php?post_type=page'); // Pages
+		remove_menu_page('plugins.php'); // Plugins
+		remove_menu_page('themes.php'); // Appearance
+		remove_menu_page('users.php'); // Users
+		remove_menu_page('tools.php'); // Tools
+		remove_menu_page('options-general.php'); // Settings
+	}
+}
+
+
+/*================================
+=            INCLUDES            =
+================================*/
+
 /**
  * Custom template tags for this theme.
  */

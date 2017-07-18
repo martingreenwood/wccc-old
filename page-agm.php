efa3d00075364bf61ff0dd292ff5749bcfe79f25
1 <martin@pixelpudu.com>
2017-07-18 11:55:42 +0100

    fix

---
+++ page-agm.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package wccc
+ */
+
+get_header(); ?>
+
+	<?php 
+	if (get_field( 'enable_t20_mode', 'option' )):
+		$bannerimage = get_template_directory_uri() . "/assets/images/rapids-banner.png";
+
+		$top_image_array = array();
+		$top_t20_images = get_field( 'header_images_t20', 'option' );
+
+		foreach ($top_t20_images as $top_t20_image) {
+			$top_image_array[] = $top_t20_image['url'];
+		}
+		$ri = array_rand($top_image_array);
+		$top_image = $top_image_array[$ri];
+	else: 
+		$bannerimage = get_template_directory_uri() ."/assets/images/banner.png";
+
+		$top_image_array = array();
+		$top_images = get_field( 'header_images', 'option' );
+		foreach ($top_images as $top_image) {
+			$top_image_array[] = $top_image['url'];
+		}
+		$ri = array_rand($top_image_array);
+		$top_image = $top_image_array[$ri];
+	endif; 
+	?>
+	<section id="jumbrotron">
+		<div class="overlay" style="background-image: url(<?php echo $bannerimage; ?>)"></div>
+
+		<div class="news" style="background-image: url(<?php echo $top_image; ?>)">
+			<div class="table"><div class="cell middle">
+				<div class="container">
+					<div class="span6">
+						<?php if (get_field( 'has_custom_title' )): ?>
+							<h1><?php the_field( 'custom_title' ); ?></h1>
+						<?php else: ?>
+							<h1><?php the_title(); ?></h1>
+						<?php endif; ?>
+					</div>
+					<div class="span6">
+					</div>
+				</div>
+			</div></div>
+		</div>
+	</section>
+
+
+	<div id="primary" class="content-area container">
+		<main id="main" class="site-main span9" role="main">
+
+			<?php
+				if ( have_posts() ) :
+					while ( have_posts() ) : the_post();
+
+						get_template_part( 'template-parts/content', 'page' );
+
+					endwhile;
+				endif; 
+			?>
+
+			<section id="files">
+			<?php
+			$args = array( 
+				'post_type' => 'wcccagm',
+				'posts_per_page' => -1,
+			);
+			$query = new WP_Query( $args );
+
+			if ( $query->have_posts() ) : 
+				while ( $query->have_posts() ): $query->the_post();
+				$pdf - get_field( 'pdf' );
+				?>
+				<a href="<?php echo $pdf['url']; ?>" class="<?php echo $pdf['type']; ?>">
+					<i class="fa fa-file-pdf-o" aria-hidden="true"></i> <?php the_title( ); ?>
+				</a>
+				<?php
+				endwhile; 
+			wp_reset_postdata(); 
+			endif; 
+			?>
+				
+			</section>
+
+		</main><!-- #main -->
+
+		<?php get_sidebar('pages'); ?>
+	</div>
+
+	<?php if( have_rows('sections') ): ?>
+	<section id="parts">
+
+	    <?php while ( have_rows('sections') ) : the_row(); ?>
+		<div class="section">
+			<div class="container">
+				<div class="row">
+				
+				    <?php while ( have_rows('column') ) : the_row(); ?>
+					<div class="column <?php the_sub_field( 'column_with' ); ?>">
+
+					<?php
+					if( have_rows('column_content') ):
+					    while ( have_rows('column_content') ) : the_row();
+
+					        if( get_row_layout() == 'content' ):
+
+					        	the_sub_field('content');
+
+					        elseif( get_row_layout() == 'image' ): 
+
+					        	$image = get_sub_field('image');
+					        	echo "<img src='".$image["url"]."'>";
+
+
+							elseif( get_row_layout() == 'video' ): 
+
+					        	the_sub_field('video');
+
+					        endif;
+
+					    endwhile;
+					endif;
+					?>
+
+					</div>
+					<?php endwhile; ?>
+					
+				</div>
+			</div>
+		</div>
+		<?php endwhile; ?>
+
+	</section>
+	<?php endif; ?>
+
+	<?php 
+		get_template_part( 'partials/sponsor', 'boxes' );
+	?>
+
+<?php
+get_footer();

a67174da261e92cd246135e26dd1c3a4a95082f5
1 <martin@pixelpudu.com>
2017-08-04 09:16:39 +0100

    fix

--- single-events.php
+++ single-events.php
@@ -41,8 +41,10 @@ get_header(); ?>
 			<div class="table"><div class="cell middle">
 				<div class="container">
 					<div class="span6">
-						<h1><?php the_title(); ?></h1>
-						<h3><?php echo date("dS F Y", strtotime(get_field( 'event_date' ))); ?></h3>
+						<h1>
+							<?php the_title(); ?><br>
+							<small><?php the_time('dS F Y'); ?></small>
+						</h1>
 					</div>
 					<div class="span6">
 					</div>
